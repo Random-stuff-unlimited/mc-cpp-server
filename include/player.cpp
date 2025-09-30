df7083090de59f649c174cafa778ce4c534281ab
root <root@delmath-fixe.localdomain>
2025-09-30 22:58:58 +0200

    init cpp

---
+++ include/player.cpp
@@ -0,0 +1,16 @@
+#ifndef PLAYER_HPP
+# define PLAYER_HPP
+
+# include <iostream>
+
+// class 
+// {
+// 	private:
+// 		std::
+
+// 	public:
+
+
+// };
+
+#endif
--- src/main.c
+++
@@ -1,7 +0,0 @@
-#include "server.h"
-
-int main()
-{
-    start_server(25565);
-    return 0;
-}
