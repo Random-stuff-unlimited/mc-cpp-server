ede2e3fe9e86bebd3cd01f03bb7e589e273d0894
delmath <delvincourtmahis@gmail.com>
2025-10-01 16:44:46 +0200

    feat: add uuid and rework packet class

---
+++ include/UUID.hpp
@@ -0,0 +1,30 @@
+#ifndef UUID_HPP
+#define UUID_HPP
+
+# include <cstdint>
+# include <string>
+
+class Buffer;
+
+class UUID {
+private:
+    uint64_t mostSigBits;
+    uint64_t leastSigBits;
+
+public:
+    UUID();
+    UUID(uint64_t most, uint64_t least);
+    ~UUID();
+
+    uint64_t getMostSigBits() const;
+    uint64_t getLeastSigBits() const;
+    void setMostSigBits(uint64_t val);
+    void setLeastSigBits(uint64_t val);
+
+    std::string toString() const;
+
+    void readFromBuffer(Buffer &buf);
+    void writeToBuffer(Buffer &buf) const;
+};
+
+#endif
--- include/packet.hpp
+++ include/packet.hpp
@@ -4,15 +4,18 @@
 # include "server.hpp"
 
 class Packet {
+	private:
+		int32_t	_size;
+		int32_t	_id;
+		Buffer	_data;
+		Player	*_player;
+		int		_socket_fd;
+
 	public:
    		Packet() = default;
 		static int readVarint(int sock);
 		static void writeVarint(int sock, int value);
 		static int varintLen(int value);
-
-		static int handleHandshake(int clientSock);
-		static void handleStatusRequest(int clientSock, Server &server);
-		static void sendDisconnectMessage(int clientSock, const std::string &reason);
 };
 
 #endif
