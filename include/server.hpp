e17091ebf413ecc7d0567dc2095d889f1a7d87c8
Antoine Mathie <antoine.mathie@ynov.com>
2025-10-01 02:43:04 +0200

    feat: Load config from json file

--- include/server.hpp
+++ include/server.hpp
@@ -2,13 +2,19 @@
 # define SERVER_HPP
 
 # include "player.hpp"
+# define ConfigFileName "config.json"
 
 class Server
 {
 	private:
-		int		_port;
-		Player	*_player_lst;
-
+		int			_port;
+		int			_protocolVersion;
+		int			_serverSize;
+		int			loadConfig();
+		Player		*_player_lst;
+		std::string	_gameVersion;
+		std::string _serverMOTD;
+		
 	public:
 		Server();
 		Server(const Server &src);
@@ -17,8 +23,14 @@ class Server
 
 		int	start_server(int port);
 
-		int		getServerPort();
-		void	setServerPort(int port);
-}
+		int			getServerPort();
+		int			getProtocolVersion();
+		int			getServerSize();
+		int			getAmountOnline();
+		void		setServerPort(int port);
+		std::string	getGameVersion();
+		std::string getServerMOTD();
+
+};
 
 #endif
--- src/server.cpp
+++ src/server.cpp
@@ -1,6 +1,10 @@
 #include "server.hpp"
+#include "include/json.hpp"
 #include <sys/socket.h>
 #include <iostream>
+#include <fstream>
+
+using json = nlohmann::json;
 
 Server::Server() {}
 Server::Server(const Server &src) {}
@@ -12,6 +16,37 @@ void	Server::setServerPort(int port) {this->_port = port;}
 
 int Server::start_server(int port)
 {
-	
+
 }
 
+int Server::loadConfig() {
+	std::ifstream inputFile(ConfigFileName);
+
+	if (!inputFile.is_open()) {
+		std::cerr << "[Server] Error: Could not open " << ConfigFileName << std::endl;
+		return 1;
+	}
+
+	json j;
+
+	try {
+		inputFile >> j;
+
+		std::cout << "[Server]: Successfully parsed " << ConfigFileName << "!" << std::endl;
+		Server::_gameVersion = j["version"]["name"];
+		Server::_protocolVersion = j["version"]["protocol"];
+		Server::_serverSize = j["maxPlayer"];
+		Server::_serverMOTD = j["serverMotd"];
+
+	} catch (json::parse_error& e) {
+		std::cerr << "[Server]: Json parse error: " << e.what() << std::endl;
+		return 1;
+	}
+	return 0;
+}
+
+std::string Server::getGameVersion() {return _gameVersion;}
+std::string Server::getServerMOTD() {return _serverMOTD;}
+int Server::getProtocolVersion() {return _protocolVersion;}
+int Server::getServerSize() {return _serverSize;}
+int Server::getAmountOnline() {return 69;}
